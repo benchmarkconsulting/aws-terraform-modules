910c9f80a4f7ef0f0951bd0d94f51dcdaba19a7a
Matt Cole <matt.s.cole@outlook.com>
2019-11-08 13:18:21 -0500

    added routes

--- vpc/main.tf
+++ vpc/main.tf
@@ -45,6 +45,6 @@ resource "aws_route_table" "main" {
 
 #Route Associated Public
 resource "aws_route_table_association" "main" {
-  subnet_id = element(aws_route_table.main.*.id, , count.index)
+  subnet_id = element(aws_subnet.main.*.id, , count.index)
   route_table_id = "${aws_route_table.main.id}"
 }
