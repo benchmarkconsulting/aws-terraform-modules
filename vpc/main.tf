4f6389a0d8956b312b2dd8c84ccd58867ec59f73
Matt Cole <matt.s.cole@outlook.com>
2019-11-08 13:50:59 -0500

    added routes

--- vpc/main.tf
+++ vpc/main.tf
@@ -74,11 +74,11 @@ resource "aws_route_table_association" "private" {
   count = length(var.private_subnets)
 
   subnet_id = element(aws_subnet.private.*.id, count.index)
-  route_table_id = element(aws_route_table.private.*.id)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
 }
 resource "aws_route_table_association" "public" {
   count = length(var.public_subnets)
 
   subnet_id = element(aws_subnet.public.*.id, count.index)
-  route_table_id = element(aws_route_table.public.*.id)
+  route_table_id = element(aws_route_table.public.*.id, count.index)
 }
