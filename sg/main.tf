8e6c88873591f6a3f7903f1c420c696ffb4509e9
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-11-08 13:18:03 -0500

    added ingress cider to main.tf

--- sg/main.tf
+++ sg/main.tf
@@ -1,17 +1,18 @@
 resource "aws_security_group" "default" {
  count              = length(var.firewall)
  name               = lookup(var.firewall[count.index], "name", "")
- vpc_id            = lookup(var.firewall[count.index], "vpc_id", "")
+ vpc_id             = lookup(var.firewall[count.index], "vpc_id", "")
 
  ingress {
-   protocol         = lookup(var.firewall[count.index], "ingress_protocol", "")
-   to_port          = lookup(var.firewall[count.index], "ingress_to_port", "")
-   from_port        = lookup(var.firewall[count.index], "ingress_from_port", "")
+   protocol             = lookup(var.firewall[count.index], "ingress_protocol", "")
+   to_port              = lookup(var.firewall[count.index], "ingress_to_port", "")
+   from_port            = lookup(var.firewall[count.index], "ingress_from_port", "")
+   ingress_cidr_blocks  = [lookup(var.firewall[count.index], "ingress_cidr_blocks", "")]
  }
   egress {
-   protocol         = lookup(var.firewall[count.index], "egress_protocol", "")
-   to_port          = lookup(var.firewall[count.index], "egress_to_port", "")
-   from_port        = lookup(var.firewall[count.index], "egress_from_port", "")
-   cidr_blocks      = [lookup(var.firewall[count.index], "cidr_blocks", "")]
+   protocol             = lookup(var.firewall[count.index], "egress_protocol", "")
+   to_port              = lookup(var.firewall[count.index], "egress_to_port", "")
+   from_port            = lookup(var.firewall[count.index], "egress_from_port", "")
+   egress_cidr_blocks   = [lookup(var.firewall[count.index], "egress_cidr_blocks", "")]
  }
 }
