3e437b9ea8bb3ca962ec9c06f4a651833d77b04e
Jeremy Carson <jeremycarson88@gmail.com>
2019-11-08 14:22:25 -0500

    updated main.tf

---
+++ nat/main.tf
@@ -0,0 +1,22 @@
+resource "aws_eip" "main" {
+  vpc = true
+}
+
+resource "aws_nat_gateway" "gw" {
+  allocation_id = aws_eip.main.id
+  subnet_id = var.public_subnet_id
+  depends_on = [aws_internet_gateway.gw]
+}
+
+resource "aws_route_table" "internet" {
+  vpc_id = var.vpc_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.gw.id
+  }
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = var.private_subnet_id
+  route_table_id = "aws_route_table.internet.id"
+}
---
+++ nat/variables.tf
@@ -0,0 +1,9 @@
+variable "allocation_id" {
+    description = "name for allocation_id"
+    type        = "string"
+}
+
+variable "subnet_id" {
+    description = "name for subnet_id"
+    type        = "string"
+}
