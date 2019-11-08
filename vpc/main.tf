9f830b3e0a7f61836289434752910fd8a7ce1703
Matt Cole <matt.s.cole@outlook.com>
2019-11-08 11:44:42 -0500

    fixing errors with routes

--- vpc/main.tf
+++ vpc/main.tf
@@ -24,27 +24,27 @@ resource "aws_subnet" "main" {
 }
 
 # Internet Gateway
-#resource "aws_internet_gateway" "main-gw" {
-#  vpc_id = "${aws_vpc.main[0].id}"
-#  tags = { 
-#    Name = "main-gw"
-#  }
-#}
+resource "aws_internet_gateway" "main-gw" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags = { 
+   Name = "main-gw"
+  }
+}
 
 # route tables
-#resource "aws_route_table" "main" {
-#  vpc_id = "${aws_vpc.main[0].id}"
-#  route {
-#    cidr_block = "0.0.0.0/0"
-#    gateway_id = "${aws_internet_gateway.main-gw.id}"
-#  }
-#  tags = { 
-#    Name = "main-public-1"
-#  }
-#}
+resource "aws_route_table" "main" {
+  vpc_id = "${aws_vpc.main.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+   gateway_id = "${aws_internet_gateway.main-gw.id}"
+  }
+  tags = { 
+    Name = "main-public-1"
+  }
+}
 
 #Route Associated Public
 #resource "aws_route_table_association" "main" {
- # subnet_id = "${aws_subnet.main[0]].id}"
- # route_table_id = "${aws_route_table.main.id}"
+#  subnet_id = "${aws_subnet.main[0]].id}"
+#  route_table_id = "${aws_route_table.main.id}"
 #}
