564edfc5b95a608ad14165ac2026d303e7d1780a
Jeremy Carson <jeremycarson88@gmail.com>
2019-11-08 15:00:34 -0500

    Sammut said put the closing bracket in the wrong spot

--- nat/main.tf
+++ nat/main.tf
@@ -23,5 +23,5 @@ resource "aws_route_table" "internet" {
 }
 resource "aws_route_table_association" "private" {
   subnet_id      = var.private_subnet_id
-  route_table_id = "aws_route_table.internet.id"
+  route_table_id = aws_route_table.internet.id
 }
