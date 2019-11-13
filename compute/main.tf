b03c58bef4163884380a59f2b4af563fb543832f
Jeremy Carson <jeremycarson88@gmail.com>
2019-11-13 13:09:19 -0500

    updated compute

--- compute/main.tf
+++ compute/main.tf
@@ -1,9 +1,9 @@
 resource "aws_instance" "harness" {
     ami = "ami-04763b3055de4860b"
     instance_type = "t2.micro"
-    subnet_id = "${vpc_id.main-public-1.id}"
-    vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
-    key_name = "${aws_key_pair.demo-key.id}"
+    #subnet_id = "${vpc_id.main-public-1.id}"
+    #vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
+    #key_name = "${aws_key_pair.demo-key.id}"
     tags = {
       Name = "bastion"
       Terraform = "True"
