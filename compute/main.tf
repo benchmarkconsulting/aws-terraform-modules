6193245ea1c363d3240f0cf3d3a4679b4077bbec
Jeremy Carson <jeremycarson88@gmail.com>
2019-11-13 13:08:30 -0500

    updated compute

--- compute/main.tf
+++ compute/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "harness" {
     ami = "ami-04763b3055de4860b"
     instance_type = "t2.micro"
-    subnet_id = "${aws_subnet.main-public-1.id}"
+    subnet_id = "${vpc_id.main-public-1.id}"
     vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
     key_name = "${aws_key_pair.demo-key.id}"
     tags = {
