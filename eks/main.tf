f31d06fad3327c31563d5d2eab05c388c4503a3c
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-11-14 14:26:17 -0500

    Inital eks build

--- eks/main.tf
+++ eks/main.tf
@@ -1,15 +1,20 @@
+resource "aws_iam_role" "test_role" {
+  name = "test_role"
+}
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = "eks-cluster-manage"
+  role       = aws_iam_role.test_role.name
 }
 
 resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
-  role       = "eks-cluster-manage"
+  role       = aws_iam_role.test_role.name
 }
+
+
 resource "aws_eks_cluster" "default" {
  name       = var.cluster_name
- role_arn           = aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy.id
+ role_arn           = aws_iam_role.test_role.arn
  vpc_config {
     subnet_ids = var.subnet_ids
   }
