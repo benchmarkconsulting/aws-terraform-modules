1d068d13a2854abd6f44e0e278483627a97df9be
Jeremy Carson <jeremycarson88@gmail.com>
2019-11-14 13:07:32 -0500

    updated variables

--- compute/main.tf
+++ compute/main.tf
@@ -1,9 +1,8 @@
 resource "aws_instance" "harness" {
     ami = var.ami
     instance_type = var.instance_type
-    #subnet_id = var.public_subnet_id
-    #vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
-    #key_name = "${aws_key_pair.demo-key.id}"
+    subnet_id = var.public_subnet_id
+    vpc_security_group_ids = ["${aws_security_group.allow-ssh.id}"]
     tags = {
       Name = "bastion"
       Terraform = "True"
