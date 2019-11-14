b9281cb1bf91577ba35d785aacbbbe04982ee4ab
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-11-14 13:51:09 -0500

    Iniital eks build

--- eks/main.tf
+++ eks/main.tf
@@ -11,5 +11,8 @@ resource "aws_eks_cluster" "default" {
  name               = var.cluster_name
  version            = var.version
  role_arn           = aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy.role
+ vpc_id             = aws_vpc.main.id
+ vpc_config {
+    subnet_ids = var.subnet_ids
   }
 }
