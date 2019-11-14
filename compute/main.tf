0a816a51f1ffb9d8092fb1f472ede319ea5122fd
Jeremy Carson <jeremycarson88@gmail.com>
2019-11-14 13:17:47 -0500

    added list(string)

--- compute/main.tf
+++ compute/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "harness" {
     ami = var.ami
     instance_type = var.instance_type
-    subnet_id = var.public_subnet_id
+    subnet_id = var.subnet_id
     vpc_security_group_ids = [var.vpc_security_group_ids]
     tags = {
       Name = "bastion"
