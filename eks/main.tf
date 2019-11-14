720916943754e459c26a7214f8557c0d3d806177
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-11-14 14:07:48 -0500

    Inital eks build

--- eks/main.tf
+++ eks/main.tf
@@ -8,7 +8,7 @@ resource "aws_iam_role_policy_attachment" "cluster_AmazonEKSServicePolicy" {
   role       = "kwdevop-eks-role"
 }
 resource "aws_eks_cluster" "default" {
- name               = var.cluster_name
+ cluster_name               = var.cluster_name
  version            = var.version
  role_arn           = aws_iam_role_policy_attachment.cluster_AmazonEKSServicePolicy.role
  vpc_id             = aws_vpc.main.id
