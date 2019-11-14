f85373997bb773be9417353562255f5f06c9bba8
Jeremy Carson <jeremycarson88@gmail.com>
2019-11-14 11:21:34 -0500

    updated compute information

--- compute/variables.tf
+++ compute/variables.tf
@@ -1,19 +1,19 @@
 
 
 variable "ami" {
-type = "string"
+type = string
 }
 
 
 variable "instance_type" {
-type = "string"
+type = string
 }
 
 variable "vpc_security_group_ids" {
-type = "string"
+type = string
 }
 
 variable "subnet_id" {
-type = "string"
+type = string
 }
 
