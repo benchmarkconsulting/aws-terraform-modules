641afc6848988ca0d19ff518209e51dcacf1c616
Jonathan Sammut <jonathansammut@Jonathans-MacBook-Pro.local>
2019-11-19 16:35:23 -0500

    initial workers.tf

--- eks/workers.tf
+++ eks/workers.tf
@@ -2,12 +2,12 @@ resource "aws_autoscaling_group" "node" {
   launch_configuration = "${aws_launch_configuration.node.id}"
   max_size             = "${var.max_size}"
   min_size             = "${var.min_size}"
-  name                 = "${car.node_name}"
+  name                 = "${var.node_name}"
   vpc_zone_identifier  = ["${var.subnet_ids}"]
 
   tag {
     key                 = "Name"
-    value               = "${car.node_name}"
+    value               = "${var.node_name}"
     propagate_at_launch = true
   }
 
