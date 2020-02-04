6d8e90da7153f773c2e7d277c26d7953307c183f
jc-benchmarkcorp <59289187+jc-benchmarkcorp@users.noreply.github.com>
2020-02-04 11:47:15 -0500

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -46,7 +46,7 @@ resource "aws_internet_gateway" "main-gw" {
 # route tables
 resource "aws_route_table" "public" {
   count = length(var.public_subnets)
-  vpc_id = "${aws_vpc.main.id}"
+  vpc_id = aws_vpc.main.id
   route {
     cidr_block = "0.0.0.0/0"
    gateway_id = aws_internet_gateway.main-gw.id
