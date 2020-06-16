7619bd107e9bef0b6559806880d21bf57be93172
om-benchmarkcorp <66264524+om-benchmarkcorp@users.noreply.github.com>
2020-06-16 12:43:19 -0400

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "gw_ip" {
-    value = aws_nat_gateway.gw.allocation_id
-    description = "The allocation id of the gateway IP address"
-}
-
-output "gw_id" {
-    value = aws_route_table.internet.route.gateway_id
-    description = "The VPC internet gatway identifier"
-}
