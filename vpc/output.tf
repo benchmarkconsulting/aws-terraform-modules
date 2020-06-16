2f58c3095e140f6d473aec51dc244ba52b72cb4b
om-benchmarkcorp <66264524+om-benchmarkcorp@users.noreply.github.com>
2020-06-16 14:07:24 -0400

    Add files via upload
    Fixed output descriptions in vpc module

--- vpc/output.tf
+++ vpc/output.tf
@@ -1,19 +1,19 @@
 output "vpc_id" {
   value       = aws_vpc.main.id
-  description = "The region where the subnets will be created"
+  description = "The region where the VPC network will be created"
 }
 
 output "subnet_public" {
   value       = aws_subnet.public.*.id
-  description = "The region where the subnets will be created"
+  description = "The region where the public subnets will be created"
 }
 
 output "subnet_private" {
   value       = aws_subnet.private.*.id
-  description = "The region where the subnets will be created"
+  description = "The region where the private subnets will be created"
 }
 
 output "internet_gw" {
   value       = aws_internet_gateway.main-gw.id
-  description = "The region where the subnets will be created"
+  description = "The region where the gateway will be created"
 }
