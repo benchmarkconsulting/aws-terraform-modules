40a1d813126170a3df67f4a62ecdc961857d676e
om-benchmarkcorp <66264524+om-benchmarkcorp@users.noreply.github.com>
2020-06-17 13:42:21 -0400

    Add files via upload

--- compute/output.tf
+++ compute/output.tf
@@ -2,3 +2,7 @@ output "subnet_id" {
     value = aws_instance.compute.subnet_id
     description = "The Id of the VPC subnet to launch the module in"
 }
+output "ami" {
+    value = aws_instance.compute.ami
+    description = "The AMI required to lauch the instance being created"
+}
